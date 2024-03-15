5cb5027daad736481f03558c510200f2a3f3ab20
a.begiashvili <a.begiashvili@oppa.ge>
2024-03-15 18:45:33 +0400

    add initial

---
+++ components/index.ts
@@ -0,0 +1,20 @@
+import CustomButton from './CustomButton'
+import Hero from './Hero'
+import Navbar from './Navbar'
+import Footer from './Footer'
+import CustomFilter from './CustomFilter'
+import SearchBar from './SearchBar'
+import SearchManufacturer from './SearchManufacturer'
+
+
+
+export { 
+    Hero,
+    Navbar,
+    Footer,
+    CustomButton,
+    CustomFilter,
+    SearchBar,
+    SearchManufacturer
+    
+}
